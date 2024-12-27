830d513a16801dc3cf0acda20f2e795ee6eb9f9b
oabuhassan <oabuhassan@sdaia.gov.sa>
2024-12-28 00:06:29 +0300

    [fix][mmul][opt] fix the code to run unsquared matrix correctly

--- src/mmult/impl/opt.c
+++ src/mmult/impl/opt.c
@@ -21,9 +21,9 @@
 // #pragma GCC optimize ("O1")
 void* impl_scalar_opt(void* args)
 {
-  args_t *parsed_args = (args_t *)args;
+    args_t *parsed_args = (args_t *)args;
 
-        float*   dest = (      float*)(parsed_args->output);
+    float*   dest = (      float*)(parsed_args->output);
   const float*   src0 = (const float*)(parsed_args->input1);
   const float*   src1 = (const float*)(parsed_args->input2);
 
@@ -35,19 +35,19 @@ void* impl_scalar_opt(void* args)
   int cols2 = parsed_args->cols2;
 
 int blockSize = 256; 
-for (int i = 0; i < rows1; i++) {
+    for (int i = 0; i < rows1; i++) {
         for (int j = 0; j < cols2; j++) {
             dest[i * cols2 + j] = 0.0f;
         }
     }
-for (int i = 0; i < rows1; i += blockSize) {
+    for (int i = 0; i < rows1; i += blockSize) {
         for (int j = 0; j < cols2; j += blockSize) {
-            for (int k = 0; k < cols2; k += blockSize) {
+            for (int k = 0; k < cols1; k += blockSize) {
                 for (int ii = i; ii < i + blockSize && ii < rows1; ii++) {
                     for (int jj = j; jj < j + blockSize && jj < cols2; jj++) {
-                        float sum = 0; 
-                        for (int kk = k; kk < k + blockSize && kk < cols2; kk++) {
-                            sum += src0[ii * cols2 + kk] * src1[kk * cols2 + jj];
+                        float sum = 0.0f;
+                        for (int kk = k; kk < k + blockSize && kk < cols1; kk++) {
+                            sum += src0[ii * cols1 + kk] * src1[kk * cols2 + jj];
                         }
                         dest[ii * cols2 + jj] += sum;
                     }
@@ -55,6 +55,6 @@ for (int i = 0; i < rows1; i += blockSize) {
             }
         }
     }
-  return NULL;
+    return NULL;
 }
 // #pragma GCC pop_options
